287dbe216934c807585e545b84d95c530d08d767
Dimpal K L <dimpalgowda20@gmail.com>
2024-11-16 22:47:20 +0530

    Java Loop Programs

---
+++ DoLoop.java
@@ -0,0 +1,10 @@
+public class DoLoop {
+    public static void main(String[] args) {
+        int x = 5;
+        do {
+            System.out.println(x);
+            x--;
+        }
+        while(x>=1);
+    }
+}
---
+++ NestedLoop.java
@@ -0,0 +1,11 @@
+public class NestedLoop {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            for(int j=5;j<=6;j++){
+                System.out.println(i + " " +j);
+            }
+        }
+
+
+    }
+}
