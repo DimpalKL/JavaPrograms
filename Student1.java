2ff37121d29c3eb77ee555b138370459e7fec173
Dimpal K L <dimpalgowda20@gmail.com>
2024-11-17 20:24:53 +0530

    OOP Programs

---
+++ Student1.java
@@ -0,0 +1,10 @@
+class Student {
+    int age=20;
+    String name="Dimple";
+
+    public static void main(String[] args) {
+        Student s= new Student();
+        System.out.println(s.age);
+        System.out.println(s.name);
+    }
+}
