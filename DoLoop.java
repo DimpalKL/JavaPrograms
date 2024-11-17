5289308a5ddc1f347b34b6e74bdbafcdd39c2c43
Dimpal K L <dimpalgowda20@gmail.com>
2024-11-17 19:43:12 +0530

    Java Loop Programs

--- DoLoop.java
+++ DoLoop.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class DoLoop {
     public static void main(String[] args) {
         int x = 5;
@@ -8,15 +7,4 @@ public class DoLoop {
         }
         while(x>=1);
     }
-=======
-public class DoLoop {
-    public static void main(String[] args) {
-        int x = 5;
-        do {
-            System.out.println(x);
-            x--;
-        }
-        while(x>=1);
-    }
->>>>>>> origin/master
 }
