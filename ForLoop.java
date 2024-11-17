14f517a0ce32db00a93fb754a92ec6e0f99d62fd
DimpalKL <dimpalgowda20@gmail.com>
2024-11-17 13:20:10 +0530

    Add files via upload
    Java loops and Pattern Programs

---
+++ ForLoop.java
@@ -0,0 +1,21 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        //hello 5 times
+        for(int i=1;i<=5;i++){
+            System.out.println("hello");
+        }//
+        //1,2,3,4,5
+        for (int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+//        //5,4,3,2,1
+        for(int i=5;i>=1;i--){
+            System.out.println(i);
+        }
+//        //2,4,6,8,10
+        for(int j=2;j<=10;j=j+2){
+            System.out.println(j);
+        }
+    }
+
+}
